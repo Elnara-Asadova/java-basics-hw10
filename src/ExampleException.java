bb29905c4fb7b48b237a33eeecb54afa901513d2
Elnara Asadova <asadova.eln@gmail.com>
2021-04-05 18:47:06 +0300

    Calculate rectangle area

--- src/ExampleException.java
+++ src/ExampleException.java
@@ -1,7 +1,19 @@
 public class ExampleException {
-  public static int rectangleArea(int a, int b) {
-    // TODO put your code here
-    return 0;
+  static void check(int firstSide,int secondSide) throws IllegalArgumentException {
+    if (firstSide < 0 || secondSide <0) {
+      throw new IllegalArgumentException("input value is below zero!");
+    }
+  }
+
+  public static int rectangleArea(int firstSide, int secondSide) {
+    try{
+      check(firstSide,secondSide);
+    }
+    catch (IllegalArgumentException e1){
+      System.err.print(e1);
+      return -1;
+    }
+    return firstSide * secondSide;
   }
 }
 
--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -6,42 +6,34 @@ import org.testng.annotations.Test;
 
 public class ExampleExceptionTest {
 
-  /**
-   * Test data for positive test.
-   *
-   * @return test data array
-   */
   @DataProvider(name = "data")
   public static Object[][] data() {
     return new Object[][]{
         {2, 2, 4},
-        {2, 3, 6}
-        // TODO add 2 more test data here
+        {2, 3, 6},
+        {1, 0, 0},
+        {5, 5, 25}
     };
   }
 
-  /**
-   * Test data for exception test.
-   *
-   * @return test data array
-   */
   @DataProvider(name = "negativeData")
   public static Object[][] negativeData() {
     return new Object[][]{
         {-2, 2},
-        {2, -2}
-        // TODO add 2 more test data here
+        {2, -2},
+        {-2, -2},
+        {0, -1}
     };
   }
 
   @Test(dataProvider = "data")
   public void testRectangleArea(int a, int b, int c) {
-    // TODO put your code here
+    assertEquals(ExampleException.rectangleArea(a,b), c, "Result is not correct");
   }
 
 
   @Test(dataProvider = "negativeData")
   public void testRectangleAreaNegative(int a, int b) {
-    // TODO put your code here
+    assertEquals(ExampleException.rectangleArea(a,b), -1, "Result is not correct");
   }
 }
